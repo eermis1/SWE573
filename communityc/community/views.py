4aef38d48883aa26a46314cfa61eafd4b3ba6ac9
Evren Ermiş <36535914+eermis1@users.noreply.github.com>
2019-11-09 00:23:45 +0300

    #6

--- communityc/community/views.py
+++ communityc/community/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 from django.template import loader
+from django.shortcuts import render
 from .models import Community
 from. models import Post
 
 
 def homepage(request):
-
     all_communities = Community.objects.all()
-    template = loader.get_template("community/index.html") 
-    context = {
-        "all_communities" : all_communities
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, "index.html", {"all_communities" : all_communities} ) #context --> all communities.
 
 def community_detail (request,community_id):
-
-    return HttpResponse("Hello, you're at the community detail page / Community :" + str(community_id))
+    try:
+        community = Community.objects.get(pk=community_id)
+    except Community.DoesNotExist:
+        raise  Http404("Community Does Not Exist")
+    return render(request, "detail.html", {"community" : community} )
