ab8573b06be0058be5c8ed215b3797b7a046575e
Evren Ermiş <36535914+eermis1@users.noreply.github.com>
2019-11-10 00:23:11 +0300

    Generic Views have been implemented fixes #10

--- communityc/community/urls.py
+++ communityc/community/urls.py
@@ -3,6 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
-    path('/<community_id>', views.community_detail, name="community_detail" )
+    path('', views.ListView.as_view(), name='homepage'),
+    path('/<pk>', views.DetailView.as_view(), name="community_detail" )
 ]
+
+#old structure
+#urlpatterns = [
+#    path('', views.homepage, name='homepage'),
+#    path('/<community_id>', views.community_detail, name="community_detail" )
+#]
--- communityc/community/views.py
+++ communityc/community/views.py
@@ -1,19 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
+from django.views import generic
 from django.template import loader
 from django.shortcuts import render
 from .models import Community
 from. models import Post
 
+class ListView(generic.ListView):
 
-def homepage(request):
-    all_communities = Community.objects.all()
-    return render(request, "index.html", {"all_communities" : all_communities} ) #context --> all communities.
+    template_name = "index.html"
+    context_object_name = "all_communities" 
+    #alttaki get_query her zaman object_list olarak return verdiğimizden index.html'deki all_communities kısmına
+    # ya object_list diyecektik ya da bu şekilde object_list --> all_communities dönüşümü yapacaktık. 
+    
+    def get_queryset(self):
+        return Community.objects.all()
 
-def community_detail (request,community_id):
-    try:
-        community = Community.objects.get(pk=community_id)
-    except Community.DoesNotExist:
-        raise  Http404("Community Does Not Exist")
-    return render(request, "detail.html", {"community" : community} )
+class DetailView(generic.DetailView):
+    model = Community #Primary Key of Lists --> Community.
+    template_name = "detail.html"
+
+
+#old structure
+#def community_detail (request,community_id):
+#    try:
+#        community = Community.objects.get(pk=community_id)
+#    except Community.DoesNotExist:
+#        raise  Http404("Community Does Not Exist")
+#    return render(request, "detail.html", {"community" : community} )
