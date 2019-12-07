ec018eda4e1e53318847ded2863c6fbd4cc4dc9e
Evren Ermiş <36535914+eermis1@users.noreply.github.com>
2019-12-07 17:11:25 +0300

    JSON Implemetation To Post Model #25

--- communityc/community/models.py
+++ communityc/community/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.postgres.fields import JSONField
 
 class Community(models.Model):
     community_name = models.CharField(max_length=100)
@@ -17,8 +18,8 @@ class Post(models.Model):
     post_title = models.CharField(max_length=100)
     post_description = models.CharField(max_length=200)
     post_tag = models.CharField(max_length=150)
+    formfield = JSONField(default = "") #The Additional/Customizable Data Fields To Be Stored In This Field
 
     def __str__ (self):
         return ("\nPost id : " + str(self.id) + "\nPost Title : " + self.post_title +  "\nPost Description : " + self.post_description +  "\nPost Tag : "  
                 + self.post_tag + "\nPost Community id : " + str(self.community))
-    
