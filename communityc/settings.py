ba85a2e64ba10aabe43bb87b64f8cdf614f82793
Evren Ermiş <36535914+eermis1@users.noreply.github.com>
2020-01-11 01:06:49 +0300

    Update settings.py

--- communityc/settings.py
+++ communityc/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
